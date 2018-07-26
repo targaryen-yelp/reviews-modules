073d7a3bc36c63b0a74081e1ace025006b6ada67
Todd Fichman <toddfichman@gmail.com>
2018-07-26 15:43:50 -0700

    Setting up CSS modules

--- client/src/ReviewList.jsx
+++ client/src/ReviewList.jsx
@@ -7,6 +7,9 @@
 import React from 'react';
 import axios from 'axios';
 import SingleReview from './SingleReview';
+import { userInfo } from 'os';
+// import CSSModules from 'reac'
+// import styles from '../'
 
 
 class ReviewList extends React.Component {
@@ -14,38 +17,15 @@ class ReviewList extends React.Component {
     super(props);
     this.state = {
       reviews: [],
-      // reviewData: {
-      //   id: '',
-      //   rating: 0,
-      //   time_created: '',
-      //   text: '',
-      //   url: '',
-      // },
-      // user: {
-      //   image_url: '',
-      //   name: '',
-      //   location: '',
-      //   friends: 0,
-      //   reviews: 0,
-      //   photos: 0,
-      //   elite: undefined,
-      // },
-      // restaurants: [],
     } 
-
-    // this.fetchReviews = this.fetchReviews.bind(this);
-    
-    
+  
   }
-
 
   componentDidMount() {
     this.fetchReviews();
-    console.log(this.state.reviews, 'what')
   }
 
   fetchReviews() {
-    const newReviews = this.state.reviews;
     axios.get('/api/reviews')
     .then(result => {
       this.setState({reviews: result.data}) 
@@ -55,24 +35,94 @@ class ReviewList extends React.Component {
   }
 
   
-  
   createData() {
-    for(let i = 0; i <= 100; i++) {
+    for(let i = 0; i < 100; i++) {
       axios.post('/api/reviews')
       .then(result => console.log(result.data))
       .catch(err => console.log(err))
     }
   }
 
+  
 
   render () {
+
+    const size = 10;
+    const reviewDisplay = this.state.reviews.slice(0, size).map((review, index) => {
+      return (
+        <li key={index} className="single-review">
+
+            <div className="user-data">
+
+              <img src={review.user.image_url} className="user-picture"/>
+              <div className="user-stats">
+
+                <div className="user-name">{review.user.name}</div>
+                <div className="user-location">{review.user.location}, CA</div>
+                <div className="user-friends">{review.user.friends} friends</div>
+                <div className="user-review-number">{review.user.reviews} reviews</div>
+                <div className="user-photo-number">{review.user.photos} photos</div>
+
+              </div>
+            
+            </div>
+
+            <br/>
+
+            <div className="review-data">
+
+              <div className="review-score-date">
+
+                <span className="review-rating">{review.reviewData.rating}</span> {'  '}
+                <span className="review-date">{review.reviewData.time_created}</span>
+
+              </div>
+
+              <div className="review-text">{review.reviewData.text}</div>
+              <img src={review.reviewData.review_pic} className="review-picture"/>
+
+              <div className="review-buttons">
+                Was this review ...?
+                <br/>
+                <button>Useful</button>
+                <button>Funny</button>
+                <button>Cool</button>
+                <button>FLAG IMAGE</button>
+              </div>
+              
+              <br/>
+             
+            </div>
+            
+        </li>
+      )
+    })
+
     return (
       <div>
-        Reviews
+        <h2>Recommended Reviews for RESTAURANT-NAME</h2>
 
-        <SingleReview 
-        reviews={this.state.reviews}
-        />
+        <div className="search-sort-language">
+          <input type="text" placeholder="Search within the reviews" />
+          Sort by
+          <select> 
+            <option value="yelp-sort">Yelp Sort</option>
+            <option value="newest">Newest First</option>
+            <option value="oldest">Oldest First</option>
+            <option value="highest-rated">Highest Rated</option>
+            <option value="lowest-rated">Lowest Rated</option>
+            <option value="elites">Elites</option>
+          </select>
+          Language
+          <select>
+            <option value="english">English ({this.state.reviews.length})</option>
+          </select>
+        </div>
+
+        <ul >
+          {reviewDisplay}
+        </ul>
+
 
         {/* <button onClick={() => this.createData()}>Create data</button> */}
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
         options: {
           presets: ["react", "env"]
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+            'style-loader?sourceMap',
+            'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+    }
     ]
   },
   resolve: {
